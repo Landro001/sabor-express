1f64136677b70304af17144b14c2ba1cd81828b4
Leandro <ljborges76@gmail.com>
2023-12-28 11:06:46 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+def exibir_nome_do_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
+
+def exibir_opcoes():
+    print('[1]. Cadastrar restaurante')
+    print('[2]. Listar restaurante')
+    print('[3]. Ativar restaurante')
+    print('[4]. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app\n')
+
+def escolher_opcaos():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case _:
+            finalizar_app()
+        
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcaos()
+
+if __name__ == '__main__':
+    main()
